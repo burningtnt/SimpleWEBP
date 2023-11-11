8520c8f4d73e86fc70375acc539b9e23220b93a8
Burning_TNT <pangyl08@163.com>
2023-11-11 16:43:42 +0800

    Fix Icon Size

--- jfx/src/main/java/net/burningtnt/webp/jfx/WEBPImageLoader.java
+++ jfx/src/main/java/net/burningtnt/webp/jfx/WEBPImageLoader.java
@@ -6,6 +6,7 @@ import com.sun.javafx.iio.ImageMetadata;
 import com.sun.javafx.iio.ImageStorage;
 import com.sun.javafx.iio.common.ImageDescriptor;
 import com.sun.javafx.iio.common.ImageLoaderImpl;
+import com.sun.javafx.iio.common.ImageTools;
 import net.burningtnt.webp.SimpleWEBPLoader;
 import net.burningtnt.webp.utils.RGBABuffer;
 
@@ -66,16 +67,21 @@ public class WEBPImageLoader extends ImageLoaderImpl {
     public ImageFrame load(int imageIndex, int width, int height, boolean preserveAspectRatio, boolean smooth) throws IOException {
         RGBABuffer.AbsoluteRGBABuffer rgbaBuffer = SimpleWEBPLoader.decodeStreamByImageLoaders(this.inputStream);
 
-        return new ImageFrame(
+        int[] outWH = ImageTools.computeDimensions(width, height, width, height, preserveAspectRatio);
+        int rWidth = outWH[0], rHeight = outWH[1];
+
+        ImageFrame imageFrame = new ImageFrame(
                 ImageStorage.ImageType.RGBA,
                 ByteBuffer.wrap(rgbaBuffer.getRGBAData()),
                 rgbaBuffer.getWidth(), rgbaBuffer.getHeight(),
                 rgbaBuffer.getWidth() * 4, null,
                 new ImageMetadata(
                         null, Boolean.FALSE, null, null, null, null, null,
-                        rgbaBuffer.getWidth(), rgbaBuffer.getHeight(),
+                        rWidth, rHeight,
                         null, null, null
                 )
         );
+
+        return width != rWidth || height != rHeight ? ImageTools.scaleImageFrame(imageFrame, rWidth, rHeight, smooth) : imageFrame;
     }
 }
